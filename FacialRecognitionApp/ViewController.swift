ffe41c60a61d3867c61f101bb1c69cac30de9bf4
Shrinal Patel <shrinalpatel98@gmail.com>
2022-04-03 22:44:13 -0400

    Initial Commit

---
+++ FacialRecognitionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacialRecognitionApp
+//
+//  Created by Shrinal Patel on 03/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
